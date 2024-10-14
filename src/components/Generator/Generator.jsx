6e8b3d7f689920a679d8ce9a6fb4b8042a02aa41
Artem <goldtree3000@gmail.com>
2024-10-14 23:28:09 +0300

    feat: add useState to generator

--- src/components/Generator/Generator.jsx
+++ src/components/Generator/Generator.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { memesData } from "../../memesData.js";
 import "./generator.css";
 
 export default function Generator() {
-	let url = "";
+	const [meme, setMeme] = useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg",
+	});
+	const [allMemeImages, setAllMemeImages] = useState(memesData);
 
-	function clickHandler() {
-		const memesDataLength = memesData.data.memes.length;
-		let randomNum = Math.floor(Math.random() * (memesDataLength - 1));
+	function getMemeImage(event) {
+		event.preventDefault();
+		const memesData = allMemeImages.data.memes;
+		const randomNum = Math.floor(Math.random() * (memesData.length - 1));
+		const url = memesData[randomNum].url;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			randomImage: url,
+		}));
+	}
 
-		url = memesData.data.memes[randomNum].url;
-		console.log(url);
+	console.log(meme);
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setMeme((prevMeme) => {
+			return { ...prevMeme, [name]: value };
+		});
 	}
 
 	return (
@@ -17,14 +35,16 @@ export default function Generator() {
 			<div className="generator">
 				<div className="text-container">
 					<div className="top-text-container">
-						<label htmlFor="top-text" className="top-text-label">
+						<label htmlFor="topText" className="top-text-label">
 							Top text
 						</label>
 						<input
 							type="text"
-							name="top-text"
+							name="topText"
 							id="top-text"
 							className="top-text-input input"
+							onChange={handleChange}
+							value={meme.topText}
 						/>
 					</div>
 					<div className="bottom-text-container">
@@ -33,15 +53,22 @@ export default function Generator() {
 						</label>
 						<input
 							type="text"
-							name="bottom-text"
-							id="bottom-text"
+							name="bottomText"
+							id="bottomText"
 							className="bottom-text-input input"
+							onChange={handleChange}
+							value={meme.bottomText}
 						/>
 					</div>
 				</div>
-				<button className="button" onClick={clickHandler}>
+				<button className="button" type="button" onClick={getMemeImage}>
 					Get a new meme image
 				</button>
+				<div className="meme">
+					<img src={meme.randomImage} alt="meme-image" className="meme-image" />
+					<h2 className="meme--text top">{meme.topText}</h2>
+					<h2 className="meme--text bottom">{meme.bottomText}</h2>
+				</div>
 			</div>
 		</main>
 	);
