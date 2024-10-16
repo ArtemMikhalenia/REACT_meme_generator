0d10d531f6da5fbbffab178b88a601b1641c303d
Artem <goldtree3000@gmail.com>
2024-10-16 18:26:51 +0300

    feat: add config vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "./",
 	plugins: [react()],
+	build: {
+		sourcemap: true,
+		outDir: "dist",
+		assetsDir: "assets",
+		cleanOutDir: true,
+	},
 	server: {
 		open: true,
 	},
